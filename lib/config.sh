4d2934de38ce5d83d6be84e93979505d0a831b36
Dominik Hassler <hadfl@omniosce.org>
2020-07-17 14:34:05 +0000

    openjdk8: update to 1.8.0_262

--- lib/config.sh
+++ lib/config.sh
@@ -166,6 +166,7 @@ UNZIP=unzip
 AWK=gawk
 GIT=git
 RIPGREP=/opt/ooce/bin/rg
+FD=/opt/ooce/bin/fd
 CMAKE=/opt/ooce/bin/cmake
 MESON_MAKE=/opt/ooce/bin/ninja
 # Command for privilege escalation. Can be overridden in site.sh
