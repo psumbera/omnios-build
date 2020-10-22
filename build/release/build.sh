ff87389116174de3763274edfbe3f841f8207795
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-10-22 15:15:03 +0000

    Preparing for r151030by

--- build/release/build.sh
+++ build/release/build.sh
@@ -23,7 +23,7 @@ SUMMARY="OmniOS release information"
 DESC="OmniOS /etc/release and /etc/os-release files"
 
 # Update the following line with the current release suffix
-RELSUFFIX=bw
+RELSUFFIX=by
 
 RELEASE=${RELVER}${RELSUFFIX}
 RELDATE="`date +%Y.%m.%d`"
