075513139003976182289bec96c514387a2f6746
Andy Fiddaman <omnios@citrus-it.co.uk>
2021-04-22 16:25:40 +0000

    OpenJDK updates

--- build/openjdk11/build.sh
+++ build/openjdk11/build.sh
@@ -18,13 +18,14 @@
 
 PROG=openjdk
 VER=11
-UPDATE=10
+UPDATE=11
 BUILD=9
 PKG=runtime/java/openjdk11
 SUMMARY="openjdk $VER"
 DESC="Open-source implementation of the eleventh edition of the Java SE Platform"
 
-LIBERATIONFONTSVER=2.1.2
+# check ooce/fonts/liberation for current version
+LIBERATIONFONTSVER=2.1.3
 SKIP_LICENCES="SILv1.1"
 
 set_arch 64
